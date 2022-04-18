60fd25d850f40765c930c2939c11806bb707556b
Siddhesh <mungekarsiddhesh2001@gmail.com>
2022-04-18 18:55:24 +0530

    sdfas

--- src/components/GroceryCards.js
+++ src/components/GroceryCards.js
@@ -1,23 +1,14 @@
 import * as React from "react";
-// import { experimentalStyled as styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-// import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-
-// const Item = styled(Paper)(({ theme }) => ({
-//   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-//   ...theme.typography.body2,
-//   padding: theme.spacing(2),
-//   textAlign: "center",
-//   color: theme.palette.text.secondary,
-// }));
-
+import {
+  Box,
+  Grid,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+  Button,
+} from "@mui/material";
 const groceryList = [
   {
     id: 1,
